2cb092a629dd140e02fb1ae95d06089aec09e571
yxclouds <w441679@gmail.com>
2026-08-22 12:06:03 +0800

    fixed type in index.css

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const DATA = [
-  {  name: "Eat", completed: true ,id: "t1"},
-  {  name: "Sleep", completed: false,id: "t2" }]
+  {  name: "Study", completed: true ,id: "t1"},
+  {  name: "Work", completed: false,id: "t2" },
+  {  name:"Fitness",completed:false,id:"t3"}]
 root.render(
   <React.StrictMode>
     <App tasks={DATA}/>
